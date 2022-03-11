d2b460afb6a06cb794500c49782e7a3de0df4189
Michael <mic.nunez@duocuc.cl>
2022-03-11 16:23:11 -0300

    suma de numeros ok

--- ejemplo.py
+++ ejemplo.py
@@ -1,3 +1,5 @@
     #este es la raiz del proyecto
     
-    X = int(input ("ingrese un numero: ")) 
+    x = int(input ("ingrese un numero: ")) 
+    y = int(input ("ingrese otro numero: "))
+    print (f"el resultado es: {x+y}")
