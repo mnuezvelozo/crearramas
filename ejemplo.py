720c72a6a4ad2660fddd5e3db1ca5c0d5cd5d7ab
Michael <mic.nunez@duocuc.cl>
2022-03-11 16:38:24 -0300

    el que esta correcto

--- ejemplo.py
+++ ejemplo.py
@@ -1,5 +1,8 @@
     #este es la raiz del proyecto
     
-    x = int(input ("ingrese un numero: ")) 
-    y = int(input ("ingrese otro numero: "))
-    print (f"el resultado es: {x+y}")
+from os import system
+system ('cls')
+
+x=int(input("ingrese un numero: "))
+y=int(input("ingrese otro numero: "))
+print (f"el resultado es {x+y}")
